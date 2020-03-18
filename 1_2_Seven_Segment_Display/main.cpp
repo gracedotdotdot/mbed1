84837bd237f2297371622583fb977d8a10f778e1
grace <gracelin0827@Gmail.com>
2020-03-18 18:05:47 +0800

    add

---
+++ 1_2_Seven_Segment_Display/main.cpp
@@ -0,0 +1,27 @@
+#include "mbed.h"
+
+
+BusOut display(D6, D7, D9, D10, D11, D5, D4, D8);
+// define the Serial object
+//Serial pc(USBTX, USBRX);
+
+char table[10] = {0x3F, 0x06, 0x5B, 0x4F, 0x66, 0x6D, 0x7D, 0x07, 0x7F, 0x6F};
+
+
+int main(){
+
+  while(1){
+
+    for (int i = 0; i<10; i = i+1){
+
+      display = table[i];
+      // Print something over the serial connection
+      //pc.printf("Blink! LED is now %d\r\n", display.read());
+
+      wait(1);
+
+    }
+
+  }
+
+}
